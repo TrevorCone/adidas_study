8bb2836799dba62ea460cde9aba29033f7b05135
TrevorCone <trevorcone44@gmail.com>
2023-03-28 01:55:37 -0400

    adidas_study

--- Adidas_Study.R
+++ Adidas_Study.R
@@ -5,6 +5,9 @@ library(ggplot2)
 library(forecast)
 library(readxl)
 library(zoo)
+library(tidyverse)
+library(dplyr)
+library(TSstudio)
 
 Adidas_US_Sales_Datasets <- read_excel("Adidas US Sales Datasets.xlsx")
 
@@ -23,7 +26,7 @@ colnamesREAL
 adidas_df <- adidas_df[-1,]
 colnames(adidas_df) <- colnamesREAL
 colnames(adidas_df)
-
+head(adidas_df)
 #The types for each row are all chr but some should be num
 # Price per, units sold, total sales, operating profit, operating margin
 adidas_df$Price_Per_Unit <- as.numeric(adidas_df$Price_Per_Unit)
@@ -40,6 +43,10 @@ tail(adidas_df)
 #See if there are any N/A values in dataframe. it returns 0 so there are
 # no N/A s
 sum(is.na.data.frame(adidas_df))
+
+adidas_df <- adidas_df[order(adidas_df$Invoice_Date),]
+head(adidas_df)
+
 ##############################################################################################################
 #Here we look at the distribution of certain Variables. To see where the
 # Most product is moved to and sold.
@@ -76,3 +83,43 @@ ggplot(adidas_df, aes(x = Sales_Method, color = Sales_Method, fill = Sales_Metho
   xlab("Sales Method")+
   ylab("Number of Sales")+
   theme(legend.position = "none")
+######################################################################
+#In sales quarters are important because sales are fluid in which the volumes of sales can differ from 
+# each quarter. For example sales can be much higher in Q4 because of popular holidays and Q1 could be slow as a result.
+# In our data set and splitting the data between each quarter for 2020 and 2021. 
+# We also see that 2020 had a lot less sales than 2021. I wonder what happened?
+
+
+Q1_2020 <- adidas_df %>% 
+  filter(between(Invoice_Date, as.Date("2020-01-01"), as.Date("2020-03-31")))
+Q2_2020 <- adidas_df %>% 
+  filter(between(Invoice_Date, as.Date("2020-04-01"), as.Date("2020-06-30")))
+Q3_2020 <- adidas_df %>%
+  filter(between(Invoice_Date, as.Date("2020-07-01"), as.Date("2020-09-30")))
+Q4_2020 <- adidas_df %>%
+  filter(between(Invoice_Date, as.Date("2020-10-01"), as.Date("2020-12-31")))
+Total_2020 <- adidas_df %>%
+  filter(between(Invoice_Date, as.Date("2020-01-01"), as.Date("2020-12-31")))
+
+Q1_2021 <- adidas_df %>% 
+  filter(between(Invoice_Date, as.Date("2021-01-01"), as.Date("2021-03-31")))
+Q2_2021 <- adidas_df %>% 
+  filter(between(Invoice_Date, as.Date("2021-04-01"), as.Date("2021-06-30")))
+Q3_2021 <- adidas_df %>%
+  filter(between(Invoice_Date, as.Date("2021-07-01"), as.Date("2021-09-30")))
+Q4_2021 <- adidas_df %>%
+  filter(between(Invoice_Date, as.Date("2021-10-01"), as.Date("2021-12-31")))
+Total_2021 <- adidas_df %>%
+  filter(between(Invoice_Date, as.Date("2021-01-01"), as.Date("2021-12-31")))
+# Q1 in 2020 and 2021
+ts_plot(Q1_2020)
+ts_plot(Q1_2021)
+# Q2 in both years
+ts_plot(Q2_2020)
+ts_plot(Q2_2021)
+# Q3 
+ts_plot(Q3_2020)
+ts_plot(Q3_2021)
+# Q4
+ts_plot(Q4_2020)
+ts_plot(Q4_2021)
